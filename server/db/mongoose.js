00d9006d33805be2b18b954198be0bb8e9a44827
robnu <brobito@gmail.com>
2018-01-14 23:56:45 -0300

    😤

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 /* Mongoose supports callbacks by default. The line below tells mongoose which promise library
 we wanna use. Here we tell mongoose we wanna use the built in promise library */
 mongoose.Promise = global.Promise;
-
+ 
 // Very similar to MongoClient.connect()
 mongoose.connect('mongodb://burno:nodecourse@ds255767.mlab.com:55767/nodejsdevcourse', {
 	useMongoClient: true
