5ac71409cb6f5a5e4d5bd6b3aba9471034627566
robnu <brobito@gmail.com>
2018-01-16 00:20:47 -0300

    ~

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -5,7 +5,8 @@ we wanna use. Here we tell mongoose we wanna use the built in promise library */
 mongoose.Promise = global.Promise;
 
 // Very similar to MongoClient.connect()
-// 'mongodb://localhost:27017/TodoApp' || mongodb://bruno:nodenodenode@ds255767.mlab.com:55767/nodejsdevcourse || 
+// 'mongodb://localhost:27017/TodoApp' || 
+process.env.MONGODB_URI = 'mongodb://bruno:nodenodenode@ds255767.mlab.com:55767/nodejsdevcourse' 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', {
 		useMongoClient: true
 	}
